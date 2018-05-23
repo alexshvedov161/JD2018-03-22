0c2a50373325e80e9906f1b45cf808f61802068c
Шведов <alexshvedov1997@gmail.com>
2018-05-23 20:06:26 +0300

    jd02_02 jd02_07 jd02_08

--- src/by/it/shvedov/jd02_06/Logger.java
+++ src/by/it/shvedov/jd02_06/Logger.java
@@ -1,31 +1,44 @@
 package by.it.shvedov.jd02_06;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Logger {
-    //private Logger() {
 
-    //}
+    private String fileName;
+    private  Logger(){fileName = getPath(Logger.class,"log.txt");}
 
-   // String path;
-      //  Logger instance;
-      //  String filename;
-      //  private Logger(String filename){
-      //      path = getPath(Logger.class, filename);
-     //   }
-      //  void getLogger() {
-       //     if (instance == null) {
+    private static Logger instance;
 
-      //      }
-     //   }
-   // private static String getPath(Class<?> cl,String clName){
-      //  String path = System.getProperty("user.dir");
-      //  path+= File.separator + "src" + File.separator;
-     //   path+=cl.getName().replace(cl.getSimpleName(),"").replace(".",File.separator);
-     //   return path+clName;
-   // }
+    static Logger getLogger(){
+        if (instance==null){
+            synchronized (Logger.class){
+                if (instance == null)
+                    instance = new Logger();
+            }
+        }
+        return instance;
+    }
+
+    void toLog(String message){
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter(fileName,true))){
+            printWriter.println(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static String getPath(Class<?> cl){
+        String path = System.getProperty("user.dir");
+        path += File.separator+"src"+File.separator;
+        path += cl.getName().replace(cl.getSimpleName(),"").replace(".",File.separator);
+        return path;
+    }
+
+    private static String getPath(Class<?>cl,String fileName){
+        return getPath(cl)+fileName;
+    }
 }
-
-
-
-
--- src/by/it/shvedov/jd02_09/TaskA.java
+++ src/by/it/shvedov/jd02_09/TaskA.java
@@ -1,25 +1,36 @@
 package by.it.shvedov.jd02_09;
 
 import javax.xml.bind.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+
+import static javax.xml.bind.Marshaller.*;
 
 public class TaskA {
     public static void main(String[] args) {
+
+
         try {
             JAXBContext context=JAXBContext.newInstance(Persons.class);
-            Unmarshaller m=  context.createUnmarshaller();
-          String xmlSource=System.getProperty("usre.dir")+"/src/by/it/shvedov/jd02_07/persons+xsd.xml";
-            String xmlTarget=System.getProperty("usre.dir")+"/src/by/it/shvedov/jd02_09/persons+xsd.xml";
-
-            // FileReader reader=new FileReader("src/by/it/shvedov/jd02_08/persons+xsd.xml");
-            Persons person=(Persons)m.unmarshal(new File(xmlSource));
-            System.out.println(person);
-
+            Unmarshaller unmarsh=context.createUnmarshaller();
+            String xmlSource=System.getProperty("user.dir")+"/src/by/it/shvedov/jd02_07/persons+xsd.xml";
+             String xmlTarget=System.getProperty("user.dir")+"/src/by/it/shvedov/jd02_09/persons+xsd.xml";
+            //String xmlTarget="/src/by/it/shvedov/jd02_07/persons+xsd.xml";
+             Persons person=(Persons) unmarsh.unmarshal(new File(xmlSource));
+             Marshaller marshaller=context.createMarshaller();
+             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,true);
+             marshaller.marshal(person,System.out);
+            try (OutputStream outputStream=new FileOutputStream(xmlTarget)){
+                marshaller.marshal(person,outputStream);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
         } catch (JAXBException e) {
             e.printStackTrace();
         }
+
+
     }
 }
